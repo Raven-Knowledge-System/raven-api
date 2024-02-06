b17b84fb3cfcf5f0b8ef268317cd44708ff62f51
Sean Hellebusch <sahellebusch@gmail.com>
2024-02-06 07:48:55 -0700

    feat: add swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Corvidae').build();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
