eaf5b71ca3d415731dea4c8bbf008112ba7bad72
Sean Hellebusch <sean.hellebusch@hingehealth.com>
2024-02-10 06:25:41 -0700

    skip

--- src/e2e/memory/post-auto.spec.ts
+++ src/e2e/memory/post-auto.spec.ts
@@ -2,8 +2,9 @@ const apiKey = 'fc1e4352-b7f0-45e0-b510-74cb1d68a913';
 
 jest.setTimeout(300000);
 
+// TODO: these tests cost me lots of API money. Figure out how to mock the API.
 describe('POST v1/memory/auto', () => {
-  it('creates a memory automatically', async () => {
+  it.skip('creates a memory automatically', async () => {
     const url = 'https://pubmed.ncbi.nlm.nih.gov/34365148/';
     const res = await fetch('http://localhost:3000/v1/memory/auto', {
       method: 'POST',
@@ -24,7 +25,7 @@ describe('POST v1/memory/auto', () => {
     });
   });
 
-  it('returns a 409 if the memory already exists', async () => {
+  it.skip('returns a 409 if the memory already exists', async () => {
     const url = 'https://pubmed.ncbi.nlm.nih.gov/34365148/';
     const res = await fetch('http://localhost:3000/v1/memory/auto', {
       method: 'POST',
